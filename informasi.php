424de9ed666498325ca2ae60c9e694195e7a958b
setiaendra18 <setiaendra18@gmail.com>
2020-03-16 10:37:18 +0700

    informasi pendaftaran

--- informasi.php
+++ informasi.php
@@ -4,14 +4,73 @@
 <div class="ui two column stackable grid">
     <div class="ten wide column">
         <div class="ui segment">
-            BIAYA KULIAH & PENDAFTARAN
+            <h3 class="ui green block header">BIAYA KULIAH</h3>
+            <div class="ui divider"></div>
+            <h4>Universitas Teknologi Mataram Jenjang Diploma 3 (D3)</h4>
+            <div class="ui bulleted list">
+                <div class="item">SPP persmester Rp. 2.450.000,-
+                    Dapat diangsur 6x dalam 1 smester, tiap bulan Rp. 410.000,-</div>
+                <div class="item">Sumbangan Pengembangan Mutu Akademik (SPMA) Rp. 6.000.000,-
+                    Bagi yang mendaftar ulang no 1 s/d 300 dapat discount 50% = Rp. 3.000.000,- dapat diangsur 12x dalam
+                    1 tahun, tiap bulan Rp. 250.000,-
+                    (SPMA = Sumbangan Pembangunan Mutu Akademik, hanya dibayar pada tahun pertama saja)
+                </div>
+                <div class="item">
+                    Biaya OSPEK Rp. 1.800.000,-
+                    Terdiri dari : seminar motivasi, 2 stel seragam, jas almamater, kaos OSPEK, biaya OSPEK, buku
+                    panduan, dll
+                </div>
+                <div class="item">
+                    <div>Total biaya yang dibayar pada saat daftar ulang Rp. 3.120.000,-</div>
+                    <div class="ui  list">
+
+                        <div class="item">SPP 2kali angsuran = 2 x Rp. 410.000,- (Rp. 820.000,-)</div>
+                        <div class="item">SPMA 2 kali angsuran = 2 Rp. 250.000,- (Rp. 500.000,-)</div>
+                        <div class="item">Biaya ospek = Rp. 1.800.000,-</div>
+                    </div>
+                </div>
+            </div>
+            <div class="ui divider"></div>
+            <h4>Universitas Teknologi Mataram Jenjang Strata 1 (S1)</h4>
+            <div class="ui bulleted list">
+                <div class="item">SPP persmester Rp. 3.240.000,-
+                    Dapat diangsur 6x dalam 1 smester, tiap bulan Rp. 540.000,-</div>
+                <div class="item">Sumbangan Pengembangan Mutu Akademik (SPMA) Rp. 6.000.000,-
+                    Bagi yang mendaftar ulang no 1 s/d 300 dapat discount 50% = Rp. 3.000.000,- dapat diangsur 12x dalam
+                    1 tahun, tiap bulan Rp. 250.000,-
+                </div>
+                <div class="item">
+                    Biaya OSPEK Rp. 1.800.000,-
+                    Terdiri dari : seminar motivasi, 2 stel seragam, jas almamater, kaos OSPEK, biaya OSPEK, buku
+                    panduan, dll
+                </div>
+                <div class="item">
+                    <div>Total biaya yang dibayar pada saat daftar ulang Rp. 3.380.000,-</div>
+                    <div class="ui  list">
+
+                        <div class="item"> SPP 2kali angsuran = 2 x Rp. 540.000,- (Rp. 1.080.000,-)</div>
+                        <div class="item">SPMA 2 kali angsuran = 2 Rp. 250.000,- (Rp. 500.000,-)</div>
+                        <div class="item">Biaya ospek = Rp. 1.800.000,-</div>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="ui segment">
-            SYARAT PENDAFTARAN
+            <h3 class="ui green block header">SYARAT PENDAFTARAN</h3>
+
+            <div class="ui ordered list">
+                <div class="item">Fotokopi Tanda kelulusan/Ijazah 1 Lembar</div>
+                <div class="item">Fotocopy KTP1 Lembar</div>
+                <div class="item">Fotocopy Kartu Keluarga 1 Lembar</div>
+                <div class="item">Pas Foto 3x4 2 Lemba</div>
+                <div class="item">Biaya Pendaftaran Rp. 250.000,-</div>
+                <a herf="form_pendaftaran.php" class="item">Mengisi formulir pendaftaran</a>
+            </div>
+
         </div>
     </div>
     <div class="six wide column">
-        <img class=" ui centered fluid image " src="assets/images/lab-jaringan.jpg"><br/>
+        <img class=" ui centered fluid image " src="assets/images/lab-jaringan.jpg"><br />
         <img class=" ui centered fluid image " src="assets/images/kelas.jpg">
     </div>
 </div>
